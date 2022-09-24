57684c477568f76dec7741baa4a12c8451bc0d1e
aadarshhegde <ahegd2@illinois.edu>
2022-09-24 13:29:42 -0500

    Dataset Added

---
+++ CrimeDataAnalyzer.py
@@ -0,0 +1,12 @@
+import csv
+import numpy
+
+
+crime_level = 0
+filename = 'pima-indians-diabetes.data.csv'
+raw_data = open(filename, 'rt')
+reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
+x = list(reader)
+data = numpy.array(x).astype('float')
+print(data.shape)
+
